5c1428c718b3fdd66684825d0dcfbd13737e8cca
lantern50 <lantern50@naver.com>
2023-11-13 17:17:59 +0900

    init: project setting

---
+++ Backend/tripjava/src/main/java/site/tripjava/tripjava/TripjavaApplication.java
@@ -0,0 +1,13 @@
+package site.tripjava.tripjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripjavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripjavaApplication.class, args);
+    }
+
+}
---
+++ Backend/tripjava/src/test/java/site/tripjava/tripjava/TripjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package site.tripjava.tripjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripjavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
