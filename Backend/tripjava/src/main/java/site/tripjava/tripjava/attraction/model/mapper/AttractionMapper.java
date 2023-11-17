0d9bf5965a3d07dc685130f9399d22b423889593
cocoiscat <head0618@naver.com>
2023-11-17 10:16:08 +0900

    feature: 여행 검색 기능 구현

--- Backend/tripjava/src/main/java/site/tripjava/tripjava/attraction/model/mapper/AttractionMapper.java
+++ Backend/tripjava/src/main/java/site/tripjava/tripjava/attraction/model/mapper/AttractionMapper.java
@@ -1,4 +1,12 @@
 package site.tripjava.tripjava.attraction.model.mapper;
 
+import site.tripjava.tripjava.attraction.model.AttractionInfo;
+import site.tripjava.tripjava.attraction.model.SearchCondition;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface AttractionMapper {
+    List<AttractionInfo> searchAttraction(SearchCondition searchCondition) throws SQLException;
+
 }
--- Backend/tripjava/src/main/java/site/tripjava/tripjava/attraction/model/service/AttractionService.java
+++ Backend/tripjava/src/main/java/site/tripjava/tripjava/attraction/model/service/AttractionService.java
@@ -1,4 +1,13 @@
 package site.tripjava.tripjava.attraction.model.service;
 
+import site.tripjava.tripjava.attraction.model.AttractionInfo;
+import site.tripjava.tripjava.attraction.model.SearchCondition;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface AttractionService {
+
+    List<AttractionInfo> searchAttraction (SearchCondition searchCondition) throws SQLException;
+
 }
