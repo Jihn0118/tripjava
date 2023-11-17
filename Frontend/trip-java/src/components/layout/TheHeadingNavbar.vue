ba44d72d63710684c35e4bfd5fdff69a492ed4d5
cocoiscat <head0618@naver.com>
2023-11-17 14:04:26 +0900

    feature: navbar 구현

---
+++ Frontend/trip-java/src/components/layout/TheHeadingNavbar.vue
@@ -0,0 +1,67 @@
+<script setup>
+import {ref} from 'vue';
+
+const selectedKeys = ref(null);
+</script>
+
+<template>
+  <div style="{padding: 20px;}">
+    <a-anchor
+        direction="horizontal"
+        :items="[
+        {
+          key: 'horizontally-part-1',
+          href: '#horizontally-part-1',
+          title: 'Part 1',
+        },
+        {
+          key: 'horizontally-part-2',
+          href: '#horizontally-part-2',
+          title: 'Part 2',
+        },
+        {
+          key: 'horizontally-part-3',
+          href: '#horizontally-part-3',
+          title: 'Part 3',
+        },
+        {
+          key: 'horizontally-part-4',
+          href: '#horizontally-part-4',
+          title: 'Part 4',
+        },
+        {
+          key: 'horizontally-part-5',
+          href: '#horizontally-part-5',
+          title: 'Part 5',
+        },
+        {
+          key: 'horizontally-part-6',
+          href: '#horizontally-part-6',
+          title: 'Part 6',
+        },
+      ]"
+    />
+  </div>
+</template>
+
+<style scoped>
+.ant-anchor-link{
+  width: 10px;
+}
+.ant-affix, .css-dev-only-do-not-override-1qb1s0s{
+
+}
+</style>
+<style>
+.ant-anchor-wrapper{
+  height: 30px;
+}
+.ant-anchor-wrapper::before{
+  border-bottom: none;
+}
+.ant-anchor-link{
+  height: 100%;
+  font-size: 20px;
+}
+</style>
+
