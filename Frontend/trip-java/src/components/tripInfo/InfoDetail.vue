7790b5671432d7e49e93257ffdc2ce63526f5cd9
cocoiscat <head0618@naver.com>
2023-11-21 11:38:55 +0900

    feature: 관광지 상세 정보 기능 구현

--- Frontend/trip-java/src/components/tripInfo/InfoDetail.vue
+++ Frontend/trip-java/src/components/tripInfo/InfoDetail.vue
@@ -1,22 +1,15 @@
 <script setup>
 
-import {ref, watch} from "vue";
+import {onMounted, ref, watch} from "vue";
 import {useRoute, useRouter} from "vue-router";
 import {infoDetail} from "@/api/attractionInfo";
 
 
 const route = useRoute();
-const Information = ref({
-  "contendId": "",
-  "title": "",
-  "image": "",
-  "latitude": 0,
-  "longitude": 0,
-  "love": 0
-})
-console.log("1234123412341234213")
+const Information = ref({})
 let {infoId} = route.params;
-console.log(infoId);
+let map;
+
 infoDetail(
     infoId,
     ({data}) => {
@@ -26,22 +19,63 @@ infoDetail(
     (error) => {
       console.log(error);
     }
-)
+);
+
+onMounted(() => {
+  const script = document.createElement("script");
+  script.src = `//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=${import.meta.env.VITE_KAKAO_MAP_SERVICE_KEY}&libraries=services,clusterer`;
+  /* global kakao */
+  script.onload = () => kakao.maps.load(() => initMap());
+  document.head.appendChild(script);
+});
+
+const initMap = () => {
+  const container = document.getElementById("map");
+  const options = {
+    center: new kakao.maps.LatLng(Information.value.latitude, Information.value.longitude),
+    level: 3,
+  };
+
+  map = new kakao.maps.Map(container, options);
+  loadMarker()
+  console.log(map);
+};
+
+const loadMarker = () => {
+  var markPosition = new kakao.maps.LatLng(Information.value.latitude, Information.value.longitude);
+  var marker = new kakao.maps.Marker({
+    position: markPosition
+  });
+  marker.setMap(map);
+}
 
 </script>
 
 <template>
   <div>
-    <h1>{{Information.title}}</h1>
-    <h2>{{Information.addr}}</h2>
+    <h1>{{ Information.title }}</h1>
+    <h2>{{ Information.addr }}</h2>
     <a-image
         :width="700"
-        :src= Information.image
+        :src=Information.image
     />
     <h1>설명</h1>
-    <p>{{Information.description}}</p>
+    <p>{{ Information.description }}</p>
+  </div>
+  <div class="map-wrap">
+    <div id="map"></div>
   </div>
 </template>
 
 <style scoped>
+.map-wrap {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#map {
+  width: 60%;
+  height: 300px;
+}
 </style>
