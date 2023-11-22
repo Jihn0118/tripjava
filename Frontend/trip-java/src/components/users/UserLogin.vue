76f824bdb211c80a1db2ab4acc0bac27764dcd23
cocoiscat <head0618@naver.com>
2023-11-22 20:26:15 +0900

    feature: 중간 작업

--- Frontend/trip-java/src/components/users/UserLogin.vue
+++ Frontend/trip-java/src/components/users/UserLogin.vue
@@ -1,15 +1,15 @@
 <script setup>
-import { ref } from "vue";
-import { storeToRefs } from "pinia";
-import { useRouter } from "vue-router";
-import { useMemberStore } from "@/stores/member";
+import {ref} from "vue";
+import {storeToRefs} from "pinia";
+import {useRouter} from "vue-router";
+import {useMemberStore} from "@/stores/member";
 
 const router = useRouter();
 
 const memberStore = useMemberStore();
 
-const { isLogin } = storeToRefs(memberStore);
-const { userLogin, getUserInfo } = memberStore;
+const {isLogin} = storeToRefs(memberStore);
+const {userLogin, getUserInfo} = memberStore;
 
 const loginUser = ref({
   memberId: "",
@@ -25,23 +25,32 @@ const login = async () => {
     router.push("/");
   }
 };
+
+const labelCol = {
+  span: 100,
+}
+
+const wrapperCol = {
+  span: 100
+}
 </script>
 
 <template>
+
   <a-form
-    :model="loginUser"
-    :label-col="{ span: 6 }"
-    :wrapper-col="{ span: 6 }"
+      :model="loginUser"
+      :label-col="labelCol"
+      :wrapper-col="wrapperCol"
   >
     <a-form-item label="아이디" name="id">
-      <a-input v-model:value="loginUser.memberId" />
+      <a-input v-model:value="loginUser.memberId"/>
     </a-form-item>
 
     <!-- :rules="[{ required: true }]" -->
     <a-form-item label="비밀번호" name="password">
       <a-input-password
-        v-model:value="loginUser.password"
-        @keyup.enter="login"
+          v-model:value="loginUser.password"
+          @keyup.enter="login"
       />
     </a-form-item>
 
@@ -49,12 +58,12 @@ const login = async () => {
       <a-checkbox>아이디 저장</a-checkbox>
     </a-form-item> -->
 
-    <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
+    <a-form-item :wrapper-col="{ offset: 10, span: 16 }">
       <a-button @click.prevent="login">로그인</a-button>
-
       <a-button style="margin-left: 5px">
         <router-link :to="{ name: 'user-join' }">회원가입</router-link>
       </a-button>
     </a-form-item>
   </a-form>
+
 </template>
