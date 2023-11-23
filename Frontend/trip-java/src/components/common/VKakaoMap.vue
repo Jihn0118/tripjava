a5461726f0d2d5d4a7548645c92099a16a1e8eb8
cocoiscat <head0618@naver.com>
2023-11-23 13:33:34 +0900

    feature: 카카오 맵 구현

--- Frontend/trip-java/src/components/common/VKakaoMap.vue
+++ Frontend/trip-java/src/components/common/VKakaoMap.vue
@@ -4,6 +4,9 @@ import {ref, watch, onMounted} from "vue";
 var map;
 const positions = ref([]);
 const markers = ref([]);
+const totalDay = ref(0);
+const lines = ref([]);
+const createLines = ref([]);
 
 const props = defineProps({stations: Array, selectStation: Object});
 
@@ -38,19 +41,24 @@ watch(
     () => props.stations,
     () => {
       positions.value = [];
+      if (props.stations.length === 0) {
+        deleteMarkers();
+        return;
+      }
 
-      // console.log("stations의 길이 = " + props.stations.length);
       const newLocation = props.stations.slice(props.stations.length - 1, props.stations.length)[0];
-      //
-
-      console.log(newLocation.lat)
-      console.log(newLocation.lng)
       moveLocation(newLocation)
 
       props.stations.forEach((station) => {
+
+        if (totalDay.value < station.day) {
+          totalDay.value = station.day;
+        }
+
         let obj = {};
         obj.latlng = new kakao.maps.LatLng(station.lat, station.lng);
         obj.title = station.statNm;
+        obj.day = station.day;
         //
         positions.value.push(obj);
       });
@@ -61,7 +69,6 @@ watch(
 
 const moveLocation = (moveValue) => {
   var moveLatLon = new kakao.maps.LatLng(moveValue.lat, moveValue.lng);
-  // console.log(moveLatLon);
   map.panTo(moveLatLon);
 }
 
@@ -76,25 +83,63 @@ const initMap = () => {
   // loadMarkers();
 };
 
+const createImage = (day) => {
+  // 마커 이미지를 생성합니다
+
+  let imgSrc = '';
+  if (day === 1) {
+    imgSrc = "src/assets/day/1.png";
+  } else if (day === 2) {
+    imgSrc = "src/assets/day/2.png";
+  } else {
+    imgSrc = "src/assets/day/3.png";
+  }
+
+
+  // 마커 이미지의 이미지 크기 입니다
+  const imgSize = new kakao.maps.Size(15, 15);
+  return new kakao.maps.MarkerImage(imgSrc, imgSize);
+}
+
+const createLine = () => {
+  for (var i = 0; i < lines.value.length; i++) {
+    var value = lines.value[i];
+    console.log("밑에 확인")
+    console.log(value);
+    var polyline = new kakao.maps.Polyline({
+      path: lines.value[i], // 선을 구성하는 좌표배열 입니다
+      strokeWeight: 2, // 선의 두께 입니다
+      strokeColor: '#000000', // 선의 색깔입니다
+      strokeOpacity: 0.5, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+      strokeStyle: 'solid' // 선의 스타일입니다
+    });
+    createLines.value.push(polyline);
+    polyline.setMap(map);
+  }
+}
+
 const loadMarkers = () => {
       // 현재 표시되어있는 marker들이 있다면 map에 등록된 marker를 제거한다.
+      deleteLines();
       deleteMarkers();
+      lines.value = [];
+      for (var i = 0; i < totalDay.value; i++) {
+        lines.value.push([]);
+      }
 
-      // 마커 이미지를 생성합니다
-      //   const imgSrc = require("@/assets/map/markerStar.png");
-      // 마커 이미지의 이미지 크기 입니다
-      //   const imgSize = new kakao.maps.Size(24, 35);
-      //   const markerImage = new kakao.maps.MarkerImage(imgSrc, imgSize);
 
       // 마커를 생성합니다
       markers.value = [];
       positions.value.forEach((position) => {
+        console.log(position)
+        lines.value[position.day - 1].push(position.latlng)
+        console.log(lines.value)
         const marker = new kakao.maps.Marker({
           map: map, // 마커를 표시할 지도
           position: position.latlng, // 마커를 표시할 위치
           // title: position.title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됨.
           clickable: true, // // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정합니다
-          // image: markerImage, // 마커의 이미지
+          image: createImage(position.day), // 마커의 이미지
         });
         markers.value.push(marker);
       });
@@ -107,6 +152,7 @@ const loadMarkers = () => {
       );
 
       map.setBounds(bounds);
+      createLine();
     }
 ;
 
@@ -115,6 +161,13 @@ const deleteMarkers = () => {
     markers.value.forEach((marker) => marker.setMap(null));
   }
 };
+
+const deleteLines = () => {
+  if (lines.value.length > 0) {
+    createLines.value.forEach((line) => line.setMap(null));
+  }
+  createLine.value = [];
+}
 </script>
 
 <template>
